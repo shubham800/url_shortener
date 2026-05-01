8ce78d7c496916ff3f6db06008697700b3d28fd3
Shubham <helloshubham567@gmail.com>
2026-05-01 17:24:45 +0530

    Install and configure Laravel Sanctum. Add database schema for companies, users, invitations, and short URLs

--- database/migrations/2026_05_01_110149_create_users_table.php
+++ database/migrations/2026_05_01_110149_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_id')->nullable()->constrained()->nullOnDelete();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['SuperAdmin', 'Admin', 'Member']);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2026_05_01_110217_create_invitations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('invited_by')->constrained('users')->cascadeOnDelete();
+            $table->string('email');
+            $table->enum('role',['Admin','Member']);
+            $table->string('token',64)->unique();
+            $table->timestamp('accepted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
